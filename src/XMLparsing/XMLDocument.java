a4cef4091b5cfda1baead63d55e18f861ac7c6c9
Nate Allen <nkallen@iastate.edu>
2018-02-11 19:11:19 -0600

    Additional XMLDocument functions

--- src/XMLparsing/XMLDocument.java
+++ src/XMLparsing/XMLDocument.java
@@ -18,6 +18,12 @@ public class XMLDocument {
         return root.getElementsByPath(path);
     }
 
+    /**
+     * Returns a list of Strings where each element is the innerXML contained in the
+     * XML element at the end of the path, and each element is unique.
+     * @param path
+     * @return
+     */
     public List<String> getUniqueValuesByPath(String path){
         List<XMLElement> temp =  root.getElementsByPath(path);
         List<String> list = new ArrayList<>();
@@ -30,6 +36,35 @@ public class XMLDocument {
 
         return list;
     }
+
+    /**
+     * Returns a list of XMLElements where each element is an element found at the end of the path
+     * and contains a key with a value equal to the one provided as an argument
+     * @param path
+     * @param key
+     * @param value
+     * @return
+     */
+    public List<XMLElement> getElementsByAttribute(String path,String key, String value){
+        List<XMLElement> temp =  root.getElementsByPath(path);
+        List<XMLElement> list = new ArrayList<>();
+
+        for(XMLElement e: temp){
+            if(value.equals(e.getAttribute(key))){
+                list.add(e);
+            }
+        }
+
+        return list;
+    }
+
+    /**
+     * Returns a list of elements where each element is an element found at the end of the path,
+     * and only the subElements with tags found in the tags[] array are populated.
+     * @param path
+     * @param tags
+     * @return
+     */
     public List<XMLElement> collect(String path, String...tags){
         List<XMLElement> list = new ArrayList<>();
 
